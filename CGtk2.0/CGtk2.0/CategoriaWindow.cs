0cc44b74f5a547871b584fca6682cdf6378f4792
roberfuentes <roberinstituto97@gmail.com>
2019-10-11 09:45:29 +0200

    Added CGTK2.0 and CSerpisAdCOnsole

---
+++ CGtk2.0/CGtk2.0/CategoriaWindow.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CGtk2
+{
+    public partial class CategoriaWindow : Gtk.Window
+    {
+        public CategoriaWindow() :
+                base(Gtk.WindowType.Toplevel)
+        {
+            this.Build();
+        }
+    }
+}
---
+++ CGtk2.0/CGtk2.0/MainWindow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Gtk;
+
+using CGtk2;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+
+        treeView.AppendColumn("id", new CellRendererText(), "text", 0);
+        treeView.AppendColumn("nombre", new CellRendererText(), "text", 1);
+        ListStore listStore = new ListStore(typeof(string), typeof(string));
+        treeView.Model = listStore;
+        listStore.AppendValues("1", "cat1");
+
+
+        newAction.Activated += (sender, e) => new CategoriaWindow();
+
+        quitAction.Activated += (sender, e) => Application.Quit();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
