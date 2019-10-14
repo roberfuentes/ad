1d4e504d8d94084739e24cf778039c39a1459c43
roberfuentes <roberinstituto97@gmail.com>
2019-10-14 08:58:39 +0200

    Using CREflexion to read the class Categoria

---
+++ CReflexion/CReflexion/Categoria.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace CReflexion
+{
+    public class Categoria
+    {
+        public Categoria(ulong id, string nombre)
+        {
+            this.id = id;
+            this.nombre = nombre;
+        }
+
+
+        private ulong id;
+        private string nombre;
+
+
+        public ulong Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+
+        }
+
+
+        }
+}
---
+++ CReflexion/CReflexion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+
+namespace CReflexion
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+
+            Categoria c = new Categoria(5, "Cat 5");
+            //Console.WriteLine("c.Id={0} c.Nombre={1}", c.Id, c.Nombre);
+
+            show(c, "Id", "Nombre");
+        }
+
+        private static void show(object obj, params string[] propertyNames)
+        {
+            foreach (string propertyName in propertyNames)
+            {
+                Console.WriteLine("Name = {0}, Value = {1}",
+                    propertyName, obj.GetType().GetProperty(propertyName).GetValue(obj));
+            }
+        }
+    }
+}
